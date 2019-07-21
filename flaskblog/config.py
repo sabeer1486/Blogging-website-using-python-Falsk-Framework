b129002ce305a22305409658583676541b8cd7e3
Shaik Sabeer <sabeer1486@gmail.com>
2019-07-21 18:34:06 +0530

    Update config.py

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,34 +1,21 @@
 
 class Config:
 	SECRET_KEY = 'cf1e97eed22864b10508978a1af8e401'
+	
+	# if you are using file system as database
 	SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+	
+	# if you are using sql server database
 	SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-      username="PostCaro",
-      password="7AM87VTghKhiAj5",
-      hostname="PostCaro.mysql.pythonanywhere-services.com",
-      databasename="PostCaro$postcaro",
+      	username="server_username",	# user sqlserver configuration here
+      	password="serve_password",	# user sqlserver configuration here
+      	hostname="server_host_name",	# user sqlserver configuration here
+      	databasename="database_name",	# user sqlserver configuration here
     )
 	SQLALCHEMY_POOL_RECYCLE = 299
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	MAIL_SERVER = 'smtp.gmail.com'
 	MAIL_PORT = 587
 	MAIL_USE_TLS = True
-	MAIL_USERNAME = 'shaiksabeer01@gmail.com'
-	MAIL_PASSWORD = '@Sweety3333'
-
-# class Config:
-# 	SECRET_KEY = 'cf1e97eed22864b10508978a1af8e401'
-# 	# SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-# 	SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-#        username="root",
-#        password="sabeer",
-#        hostname="127.0.0.1",
-#        databasename="postcaro",
-#     )
-# 	SQLALCHEMY_POOL_RECYCLE = 299
-# 	SQLALCHEMY_TRACK_MODIFICATIONS = False
-# 	MAIL_SERVER = 'smtp.gmail.com'
-# 	MAIL_PORT = 587
-# 	MAIL_USE_TLS = True
-# 	MAIL_USERNAME = 'shaiksabeer01@gmail.com'
-# 	MAIL_PASSWORD = '@Sweety3333'
+	MAIL_USERNAME = '************@gmail.com' # enter your email for password reset request
+	MAIL_PASSWORD = '*******' # your gamil password for sending password reset mail from your mail
